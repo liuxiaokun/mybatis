64eac4ecdd53a46b59f55077b5c1c2aec79308a9
Fred Liu <liuxiaokun0410@qq.com>
2015-06-04 15:54:30 +0800

    first demo

---
+++ src/main/java/com/fred/mybatis/demo1/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.fred.mybatis.demo1;
+
+import org.apache.ibatis.session.SqlSession;
+
+/**
+ * Created by liuxiaokun on 2015/6/4.
+ */
+public class HelloWorld {
+
+    public static void main(String args[]) {
+
+
+
+        SqlSession session = SessionFactory.getInstance();
+
+
+
+        }
+}
---
+++ src/main/java/com/fred/mybatis/demo1/SessionFactory.java
@@ -0,0 +1,26 @@
+package com.fred.mybatis.demo1;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by liuxiaokun on 2015/6/4.
+ */
+public class SessionFactory {
+
+
+    public static SqlSession getInstance() throws IOException {
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+
+        return session;
+    }
+}
